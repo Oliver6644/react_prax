ff9651b313fbaf21b6901dae0f356f432924d439
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 13:47:37 +0200

    ep .26 understanting keys

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,7 @@
-export default function Product(){
-   const products = ["Laptop","Phone","Modem","Computer"];
-   const productList = products.map((product) => <h3>{product}</h3>);
+export default function Product() {
+   const products = ["Laptop","Phone","Modem","Computer","Laptop"];
+   const productList = products.map((product) => (
+   <h3 key={product}>{product}</h3>
+   ));
    return <div>{productList}</div>
 };

8c596ac6a416a62dec0104fbef5942403d1f3a8f
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 11:11:47 +0200

    ep .17 Event Handling in functional components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Message from "./components/Message";
 import Profile from "./components/Profile";
 import Counter from "./components/Counter";
 import Resume from "./components/Resume";
+import FunctionEvent from './components/FunctionEvent';
 function App() {
   return (
     <div className="App">
-     <Resume name="Oliver"></Resume>
+     <FunctionEvent></FunctionEvent>
     </div>
   );
 }
---
+++ src/components/FunctionEvent.js
@@ -0,0 +1,13 @@
+function FunctionEvent(){
+    const handleClick = ()=>{
+        console.log("Button clicked!");
+    }
+    return(
+        <div>
+           Functional components
+           <button onClick={handleClick}>Click here</button>
+        </div>
+    );
+}
+
+export default FunctionEvent;

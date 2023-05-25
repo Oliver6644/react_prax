c7ce6050fe1bf840f12dc9f4d3757fa9ece3f82a
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 17:21:18 +0200

    ep .30 handling user input part 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,19 @@ import { Component } from "react";
 class Form extends Component{
      state ={
        firstname:"", 
+       lastname:"",    
     };
     handleChange = (event) => {
        this.setState({
          firstname: event.target.value,
        });
     };
+
+    handleLastNameChange =(event)=>{
+        this.setState({
+            lastname:event.target.value,
+        });
+    }
     render() {
     return <div>Form
         <form>
@@ -16,6 +23,8 @@ class Form extends Component{
            type="text"
            value={this.state.firstname}
            ></input>
+
+           <input value={this.state.lastname} type="text" onChange={this.handleLastNameChange}></input>
         </form>
     </div>
     }

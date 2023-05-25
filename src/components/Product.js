c649967bb061b98dad1df7c5b062702b8771dd1b
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 15:06:11 +0200

    ep .27 rendering list of objects

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,14 @@
 export default function Product() {
-   const products = ["Laptop","Phone","Modem","Computer","Laptop"];
+   //const products = ["Laptop","Phone","Modem","Computer","Laptop"];
+   const products = [
+      {id:1,name:"Laptop",price:500 },
+      {id:2,name:"Phone",price:200 },
+      {id:3,name:"Modem",price:50 },
+      {id:4,name:"Laptop",price:900 },
+   ];
    const productList = products.map((product) => (
-   <h3 key={product}>{product}</h3>
+   <h3 key={product.id}>
+      {product.name}: ${product.price}</h3>
    ));
    return <div>{productList}</div>
 };

1f7e2361f0266914409fe48090ea1361cabb07e6
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 13:07:58 +0200

    ep .22 conditional rendering using elements

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -1,20 +1,11 @@
 import { useState } from "react";
 export default function ConditionalCompoment(){
      const [display,setDisplay] = useState(true);
+    let output;
     if(display){
-     return (
-     <div>
-       <h3>This is a conditional component</h3>
-     </div>
-    );
-    } else {
-        return(
-        <div>
-       <h3>Nothing to see here</h3>
-     </div>
-    );
-    }   
-    
-
-    
+       output = <h3>This is a conditional component</h3>;
+    }else{
+       output = <h3>Nothing to see here</h3>
+    }
+    return <div>{output}</div>;
 }

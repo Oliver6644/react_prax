7a49d8a55bf503486405486239b543e87d9fe8a8
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 13:39:00 +0200

    ep .25 list rendering

---
+++ src/components/Product.js
@@ -0,0 +1,5 @@
+export default function Product(){
+   const products = ["Laptop","Phone","Modem","Computer"];
+   const productList = products.map((product) => <h3>{product}</h3>);
+   return <div>{productList}</div>
+};

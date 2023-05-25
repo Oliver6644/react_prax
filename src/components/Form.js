34cb85e07243820968570d1c6a8b1c0236ad582a
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 17:29:07 +0200

    ep .31 form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,17 +14,29 @@ class Form extends Component{
         this.setState({
             lastname:event.target.value,
         });
-    }
+    };
+    handleSubmit =(event)=>{
+        event.preventDefault();
+    console.log({
+       fname: this.state.firstname,
+       lname: this.state.lastname,
+    })
+    };
     render() {
     return <div>Form
-        <form>
+        <form onSubmit={this.handleSubmit}>
            <input 
            onChange ={this.handleChange} 
            type="text"
            value={this.state.firstname}
            ></input>
 
-           <input value={this.state.lastname} type="text" onChange={this.handleLastNameChange}></input>
+           <input 
+           value={this.state.lastname}
+           type="text"
+           onChange={this.handleLastNameChange}
+           ></input>
+           <button type="submit">Submit</button>
         </form>
     </div>
     }

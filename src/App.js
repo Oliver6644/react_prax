f94b5b1062a55bd96bd706f6f8ea4ba2c50fac58
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 12:12:13 +0200

    ep .18 event handling in class based components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Profile from "./components/Profile";
 import Counter from "./components/Counter";
 import Resume from "./components/Resume";
 import FunctionEvent from './components/FunctionEvent';
+import ClassEvent from "./components/ClassEvent";
 function App() {
   return (
     <div className="App">
-     <FunctionEvent></FunctionEvent>
+     <ClassEvent></ClassEvent>
     </div>
   );
 }

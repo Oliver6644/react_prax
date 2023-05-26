7194af98f4e0bde396f5e0c76e40f33741ae1229
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-26 10:07:23 +0200

    fixing errors

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "./App.css";
 //import Hello from './components/Hello';
 //import Message from "./components/Message";
 //import Profile from "./components/Profile";
-import Form from "./components/Form";
-import Todo from "./components/Todo";
-import Counter from "./components/Counter";
-import FunctionalCounter from "./components/FunctionalCounter";
+//import Form from "./components/Form";
+//import Todo from "./components/Todo";
+//import Counter from "./components/Counter";
+//import FunctionalCounter from "./components/FunctionalCounter";
 //import Resume from "./components/Resume";
 //import FunctionEvent from './components/FunctionEvent';
 //import ClassEvent from "./components/ClassEvent";
-import ConditionalCompoment from "./components/ConditionalComponent";
-import Product from "./components/Product";
-import InlineComponent from "./components/InlineComponent";
-import StylesheetComponent from "./components/StylesheetComponent";
+//import ConditionalCompoment from "./components/ConditionalComponent";
+//import Product from "./components/Product";
+//mport InlineComponent from "./components/InlineComponent";
+//import StylesheetComponent from "./components/StylesheetComponent";
 import ModuleComponent from "./components/ModuleComponent";
 function App() {
   return (
